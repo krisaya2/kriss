85e82a8c6857b99e933ac78c7e344d3180f2af91
krisaya2 <81916142+krisaya2@users.noreply.github.com>
2021-04-04 22:35:00 +0800

    Add files via upload

---
+++ index_files/click.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('e2ktsl3y') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.uclick = "e2ktsl3y";  tokens.uclickhash = "e2ktsl3y-e2ktsl3y-6j3y-0-6j3y-17zw-17j6-ec9a82"; 
